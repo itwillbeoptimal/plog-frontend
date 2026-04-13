a3a69038294d16c9aabaae578778f97a35d4db48
지훈 <153643266+itwillbeoptimal@users.noreply.github.com>
2026-04-13 16:08:24 +0900

    chore: ESLint import 정렬 커스텀 그룹 설정 및 적용 (#14)

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -8,7 +8,12 @@ export default [
       'simple-import-sort': simpleImportSort,
     },
     rules: {
-      'simple-import-sort/imports': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [['^\\u0000'], ['^react'], ['^@?\\w'], ['^@/'], ['^\\.']],
+        },
+      ],
       'simple-import-sort/exports': 'error',
     },
   },
