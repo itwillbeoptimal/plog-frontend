df141d8953f6302056c78f885a221b52eb88dd70
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-04-15 13:10:32 +0900

    feat: Switch 공통 컴포넌트 제작 (#22)
    * feat: Switch 컴포넌트 구현
    
    * Chore : Switch 컴포넌트 story 문서 작성
    
    * chore: Switch 컴포넌트에서 defaultChecked 속성 제거
    
    * fix: SwitchProps 타입 수정 및 Switch 스토리북 실시간 동작 반영
    
    * fix: Switch 컴포넌트의 focus-visible 스타일 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,5 +3,6 @@ export { default as Button } from './components/Button';
 export { default as Dialog } from './components/Dialog';
 export { default as Field } from './components/Field';
 export { default as Input } from './components/Input';
+export { default as Switch } from './components/Switch';
 export { default as Textarea } from './components/Textarea';
 export * from './tokens';
